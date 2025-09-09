d21fb336c2ccccfcee0fc336b2fd9095f8752c75
windowwelder <welderules@duck.com>
2025-09-09 17:54:21 +0700

    type: AriaLiveStatus component

--- components/AriaLiveStatus.tsx
+++ components/AriaLiveStatus.tsx
@@ -1,9 +1,19 @@
+import type {JSX} from 'react'
+
+type AriaLiveStatusProps = {
+    currentWord: string,
+    lastGuessedLetter: string,
+    guessedLetters: string[],
+    numGuessesLeft: number
+}
+
 export default function AriaLiveStatus({
                                            currentWord,
                                            lastGuessedLetter,
                                            guessedLetters,
                                            numGuessesLeft
-                                       }) {
+                                       }:AriaLiveStatusProps)
+                                       : JSX.Element {
     return (
         <section
             className="sr-only"
@@ -18,7 +28,7 @@ export default function AriaLiveStatus({
                 You have {numGuessesLeft} attempts left.
             </p>
             <p>
-                Current word: {currentWord.split("").map(letter =>
+                Current word: {currentWord.split("").map((letter: string) =>
                 guessedLetters.includes(letter) ? letter + "." : "blank."
             ).join(" ")}
             </p>
