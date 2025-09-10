b34bdadb34306d7e099cc38a60fa0c911b224d06
windowwelder <welderules@duck.com>
2025-09-10 10:56:05 +0700

    type: WordLetters

--- components/WordLetters.tsx
+++ components/WordLetters.tsx
@@ -1,11 +1,18 @@
 import { clsx } from "clsx"
+import type {JSX} from 'react'
 
-export default function WordLetters({ currentWord, guessedLetters, isGameLost }) {
+type WordLettersProps = {
+    currentWord: string, 
+    guessedLetters: string[], 
+    isGameLost: boolean
+}
+
+export default function WordLetters({ currentWord, guessedLetters, isGameLost }: WordLettersProps): JSX.Element {
     return (
         <section className="word">
-            {currentWord.split("").map((letter, index) => {
-                const shouldRevealLetter = isGameLost || guessedLetters.includes(letter)
-                const letterClassName = clsx(
+            {currentWord.split("").map((letter: string, index: number): JSX.Element => {
+                const shouldRevealLetter: boolean = isGameLost || guessedLetters.includes(letter)
+                const letterClassName: string = clsx(
                     isGameLost && !guessedLetters.includes(letter) && "missed-letter"
                 )
                 return (
