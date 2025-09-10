da406c812230977c1a2e2587c692e1b29324872b
windowwelder <welderules@duck.com>
2025-09-10 11:06:19 +0700

    type: NewGameButtonProps function

--- components/NewGameButton.tsx
+++ components/NewGameButton.tsx
@@ -1,4 +1,11 @@
-export default function NewGameButton({ isGameOver, startNewGame }) {
+import type {JSX} from 'react'
+
+type NewGameButtonProps ={ 
+    isGameOver: boolean,
+    startNewGame: () => void
+}
+
+export default function NewGameButton({ isGameOver, startNewGame }): JSX.Element | null {
     if (!isGameOver) {
         return null
     } else {
