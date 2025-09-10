43dbeba7d5ea92612f742f78797a866ba737ce99
windowwelder <welderules@duck.com>
2025-09-10 11:18:37 +0700

    type: Keyboard function

--- components/Keyboard.tsx
+++ components/Keyboard.tsx
@@ -1,4 +1,14 @@
 import { clsx } from "clsx"
+import type { JSX } from 'react'
+
+type KeyboardProps = {
+    alphabet: string,
+    guessedLetters: string[],
+    currentWord: string,
+    isGameOver: boolean,
+    addGuessedLetter: (parameter: string) => void
+}
+
 
 export default function Keyboard({
                                      alphabet,
@@ -6,12 +16,12 @@ export default function Keyboard({
                                      currentWord,
                                      isGameOver,
                                      addGuessedLetter
-                                 }) {
-    const keyboardElements = alphabet.split("").map(letter => {
-        const isGuessed = guessedLetters.includes(letter)
-        const isCorrect = isGuessed && currentWord.includes(letter)
-        const isWrong = isGuessed && !currentWord.includes(letter)
-        const className = clsx({
+                                 }: KeyboardProps): JSX.Element {
+    const keyboardElements: JSX.Element[] = alphabet.split("").map((letter: string): JSX.Element => {
+        const isGuessed: boolean = guessedLetters.includes(letter)
+        const isCorrect: boolean = isGuessed && currentWord.includes(letter)
+        const isWrong: boolean = isGuessed && !currentWord.includes(letter)
+        const className: string = clsx({
             correct: isCorrect,
             wrong: isWrong
         })
